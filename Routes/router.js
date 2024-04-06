c796e9107f80a8e0ef258ea26360bfd6528f8f56
unknown <akshatgoyal2002n@gmail.com>
2024-04-06 10:00:18 +0530

    first commit

--- Routes/router.js
+++ Routes/router.js
@@ -1,10 +1,38 @@
+// const express = require("express");
+// const router = new express.Router();
+// const controllers = require("../controllers/userControllers");
+
+
+
+// router.post("/user/register",controllers.userregister);
+// router.post("/user/sendotp",controllers.userOtpSend);
+
+// module.exports = router;
+
+
+
 const express = require("express");
 const router = new express.Router();
 const controllers = require("../controllers/userControllers");
 
+router.post("/user/register", async (req, res) => {
+  try {
+    const result = await controllers.userregister(req.body); // Assuming userregister is an async function
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+});
 
-
-router.post("/user/register",controllers.userregister);
-
+router.post("/user/sendotp", async (req, res) => {
+  try {
+    const result = await controllers.userOtpSend(req.body); // Assuming userOtpSend is an async function
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+});
 
 module.exports = router;
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,34 +1,97 @@
 const users = require("../models/userSchema");
 const userotp = require("../models/userOtp");
+const nodemailer = require("nodemailer");
 
-exports.userregister = async(req,res)=>{
-    const {email,
-         phoneNo ,
-         userName} = req.body;
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL,
+        // pass: process.env.PASSWORD
+    }
+})
 
-         if(!email ||!phoneNo ||!userName){
-            res.status(400).json({error:"Please enter all input data"})
-         }
+exports.userregister = async (req, res) => {
+    const { email, phoneNo, userName } = req.body;
 
+    if (!email || !phoneNo || !userName) {
+        return res.status(400).json({ error: "Please enter all input data" });
+    }
 
-         try {
-            const presuer = await users.findOne({email:email});
+    try {
+        const presuer = await users.findOne({ email: email });
 
-            if(presuer){
-                res.status(400).json({error:"This user Allready exist in our db"})
-            }
-            else{
-                const userregister = new users({
-                    email,phoneNo,userName
-                });
-
-                const storeData = await userregister.save();
-                res.status(200).json(storeData);
-            }
-         } catch (error) {
-            res.status(400).json({error:"Invalid Details",error})
-         }
+        if (presuer) {
+            return res.status(400).json({ error: "This user already exists in our db" });
+        } else {
+            const userregister = new users({ email, phoneNo, userName });
+            const storeData = await userregister.save();
+            return res.status(200).json(storeData);
+        }
+    } catch (error) {
+        return res.status(400).json({ error: "Invalid Details", error });
+    }
 }
 
+exports.userOtpSend = async (req, res) => {
+    const { email } = req.body;
 
+    if (!email) {
+        return res.status(400).json({ error: "Please enter your email" });
+    }
 
+    try {
+        const presuer = await users.findOne({ email: email });
+
+        if (!presuer) {
+            return res.status(400).json({ error: "This user does not exist in our db" });
+        }
+
+        const OTP = Math.floor(100000 + Math.random() * 900000);
+
+        const existEmail = await userotp.findOne({ email: email });
+
+        if (existEmail) {
+            const updateData = await userotp.findByIdAndUpdate({ _id: existEmail._id }, { otp: OTP }, { new: true });
+
+            const mailOptions = {
+                from: process.env.EMAIL,
+                to: email,
+                subject: "Sending Email For Otp Validation",
+                text: OTP
+            }
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log("error", error);
+                    return res.status(400).json({ error: "email not sent" });
+                } else {
+                    console.log("Email sent", info.response);
+                    return res.status(200).json({ message: "Email sent successfully" });
+                }
+            });
+        } else {
+            const saveOtpData = new userotp({ email, otp: OTP });
+            await saveOtpData.save();
+
+            const mailOptions = {
+                from: process.env.EMAIL,
+                to: email,
+                subject: "Sending Email For Otp Validation",
+                text:  OTP
+            }
+
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log("error", error);
+                    return res.status(400).json({ error: "email not sent" });
+                } else {
+                    console.log("Email sent", info.response);
+                    return res.status(200).json({ message: "Email sent successfully" });
+                }
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "An error occurred" });
+    }
+}
